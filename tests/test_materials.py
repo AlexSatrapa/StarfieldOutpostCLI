18d3f310853a400d34a4db431915a91763474254
Alex Satrapa <grail@goldweb.com.au>
2024-03-09 21:47:56 +1100

    Recognise leading count and trailing labels

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -27,6 +27,7 @@ class DescribeMaterialsModule(unittest.TestCase):
         {
             'name': 'Wind Turbine - Advanced',
             'count': 2,
+            'label_text': None,
             'materials': {
                 'Aluminum': 5,
                 'Isocentered Magnet': 2,
@@ -39,6 +40,7 @@ class DescribeMaterialsModule(unittest.TestCase):
         {
             'name': 'Landing Pad - Small',
             'count': 1,
+            'label_text': None,
             'materials': {
                 'Aluminum': 8,
                 'Iron': 20,
@@ -51,6 +53,7 @@ class DescribeMaterialsModule(unittest.TestCase):
         {
             'name': 'Industrial Workbench',
             'count': 1,
+            'label_text': None,
             'materials': {
                 'Aluminum': 4,
                 'Iron': 3,
@@ -109,3 +112,50 @@ class DescribeMaterialsModule(unittest.TestCase):
 """.strip()
         condensed = materials.condense(expanded).strip()
         self.assertEqual( condensed, specification )
+
+class DescribeItemNameMatchingRegex(unittest.TestCase):
+    def test_matches_undecorated_item_name(self):
+        item_name = 'Wind Turbine - Advanced'
+        match_result = materials.item_name_regex.fullmatch(item_name)
+        self.assertEqual(match_result['item_name'], item_name)
+
+    def test_matches_item_name_with_number_in_front(self):
+        item_text = '2 Wind Turbine - Advanced'
+        expected = {
+        'item_name': 'Wind Turbine - Advanced',
+        'item_count': '2',
+        }
+        match_result = materials.item_name_regex.fullmatch(item_text)
+        found = {
+            'item_name': match_result['item_name'],
+            'item_count': match_result['item_count'],
+        }
+        self.assertEqual(found, expected)
+
+    def test_matches_item_name_with_power_label(self):
+        item_text = 'Wind Turbine - Advanced (12 power)'
+        expected = {
+            'item_name': 'Wind Turbine - Advanced',
+            'labels': '(12 power)',
+        }
+        match_result = materials.item_name_regex.fullmatch(item_text)
+        found = {
+            'item_name': match_result['item_name'],
+            'labels': match_result['labels'],
+        }
+        self.assertEqual(found, expected)
+
+    def test_matches_item_name_with_count_and_labels(self):
+        item_text = '2 Wind Turbine - Advanced (12 power)'
+        expected = {
+            'item_name': 'Wind Turbine - Advanced',
+            'item_count': '2',
+            'labels': '(12 power)',
+        }
+        match_result = materials.item_name_regex.fullmatch(item_text)
+        found = {
+            'item_name': match_result['item_name'],
+            'item_count': match_result['item_count'],
+            'labels': match_result['labels'],
+        }
+        self.assertEqual(found, expected)
